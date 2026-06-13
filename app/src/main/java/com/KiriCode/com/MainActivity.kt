ec9e5e5b125bfc60c000a3b7a4d18eb859b0b202
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-13 15:31:02 +0530

    Update MainActivity.kt

--- app/src/main/java/com/KiriCode/com/MainActivity.kt
+++ app/src/main/java/com/KiriCode/com/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.KiriCode.com
 
+import android.os.Bundle
 import android.view.View
-import com.google.androidgamesdk.GameActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : GameActivity() {
-    companion object {
-        init {
-            System.loadLibrary("com")
-        }
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        hideSystemUi()
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        webSettings.databaseEnabled = true
+        
+        webView.isVerticalScrollBarEnabled = false
+        webView.isHorizontalScrollBarEnabled = false
+
+        webSettings.useWideViewPort = true
+        webSettings.loadWithOverviewMode = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("file:///android_asset/index.html")
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -25,5 +54,13 @@ class MainActivity : GameActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
